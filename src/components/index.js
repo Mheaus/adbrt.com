2a590827f3fb877e99b06d7ef41a1967260f468a
mathieu audebert <mathieu.adbrt@gmail.com>
2019-03-30 15:18:28 +0100

    gatsby init and import old files

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './header';
+import Image from './image';
+import Layout from './layout';
+import SEO from './seo';
+
+export { Header, Image, Layout, SEO };
---
+++ src/pages/page-2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout, SEO } from '../components';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+  </Layout>
+);
+
+export default SecondPage;

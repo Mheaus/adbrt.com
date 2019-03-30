328a361a8473f6f5a84460f2c233530686dde078
mathieu audebert <mathieu.adbrt@gmail.com>
2019-03-30 15:33:33 +0100

    feat : update layout css

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,18 +15,7 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{typeof children === 'function' ? children(data) : children}</main>
-      </div>
-    )}
+    render={data => <main>{typeof children === 'function' ? children(data) : children}</main>}
   />
 );
 

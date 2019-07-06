eddd5b9cac6228bb9904cb4f14c555fcfcd83137
Mathieu Audebert <mathieu.audebert@timeonegroup.com>
2019-07-06 15:35:53 +0200

    feat: change home colors and add an test pixi page

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import Nav from './Nav';
 import NavLayout from './Nav/NavLayout';
 import RadialBackground from './RadialBackground';
 import SEO from './seo';
+import SocialLinkIcon from './SocialLinkIcon';
 import Welcome from './Welcome';
 
-export { Header, Image, Layout, Nav, NavLayout, RadialBackground, SEO, Welcome };
+export { Header, Image, Layout, Nav, NavLayout, RadialBackground, SEO, SocialLinkIcon, Welcome };

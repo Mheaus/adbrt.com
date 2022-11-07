6ab31176ac4328ceff0f160533bc076606a641bf
Mathieu Audebert <mathieu.adbrt@gmail.com>
2022-11-07 13:10:08 +0100

    commenting jony-dep

--- src/pages-components/jony-dep/Room.tsx
+++ src/pages-components/jony-dep/Room.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
-import { useObjectVal } from 'react-firebase-hooks/database';
-import * as firebase from 'gatsby-plugin-firebase';
+// import { useObjectVal } from 'react-firebase-hooks/database';
+// import * as firebase from 'gatsby-plugin-firebase';
 
-import { useLocalStorage } from '../../hooks';
+// import { useLocalStorage } from '../../hooks';
 
-function createGame({ name = '' }: { name?: string }) {
-  firebase.database().ref(`games`).push({});
-}
+// function createGame({ name = '' }: { name?: string }) {
+//   firebase.database().ref(`games`).push({});
+// }
 
 interface RoomProps {
   gameId: string;
 }
 
 const Room: React.FC<RoomProps> = (props) => {
-  const { gameId } = props;
-  const [game, isLoading] = useObjectVal<{ name: string }[]>(firebase.database().ref(`game${gameId}`));
+  // const { gameId } = props;
+  // const [game, isLoading] = useObjectVal<{ name: string }[]>(firebase.database().ref(`game${gameId}`));
 
-  if (isLoading) {
-    return <span>chargement en cours...</span>;
-  }
+  // if (isLoading) {
+  //   return <span>chargement en cours...</span>;
+  // }
 
   return <div></div>;
 };

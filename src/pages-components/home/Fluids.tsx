f78cae6a4f2219037c6018a19f41869fed41cde9
Mathieu Audebert <mathieu.adbrt@gmail.com>
2023-10-04 16:03:48 +0200

    wip

--- src/pages-components/home/Fluids.tsx
+++ src/pages-components/home/Fluids.tsx
@@ -1,99 +1,139 @@
+import * as React from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { useLayoutEffect, useRef } from 'react';
 
+const vertexShader = `
+uniform float uTime;
+uniform vec2 uMouse;
+uniform vec2 uResolution;
+
+varying vec2 vUv;
+
+void main() {
+  vec3 pos = position.xyz;
+
+  // // Apply fluid transformation
+  // float noise1 = cnoise(vec3(pos * 0.8, uTime * 0.2));
+  // float noise2 = cnoise(vec3(pos * 1.5, uTime * 0.5));
+  // float noise3 = cnoise(vec3(pos * 2.5, uTime * 0.3));
+  // vec3 noiseVec = vec3(noise1, noise2, noise3);
+  // pos += noiseVec * 0.1;
+
+  // // Add mouse displacement
+  // float dist = distance(gl_Vertex.xy / uResolution.xy, uMouse);
+  // pos.z += smoothstep(0.2, 0.0, dist) * 0.5;
+
+  // Set position and UV
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+}
+`;
+
+const fragmentShader = `
+uniform float uTime;
+uniform vec2 uResolution;
+uniform float uDensity;
+uniform float uWeight;
+uniform float uRadius;
+uniform vec3 uColor1;
+uniform vec3 uColor2;
+uniform vec3 uColor3;
+
+varying vec2 vUv;
+
+void main() {
+  vec3 color = vec3(0.0);
+
+  // Calculate fluid density
+  for (float x = -uRadius; x < uRadius; x += uWeight) {
+    for (float y = -uRadius; y < uRadius; y += uWeight) {
+      float dist = length(vec2(x, y));
+      if (dist < uRadius) {
+        float density = 1.0 - pow(dist / uRadius, 2.0);
+        density *= uDensity;
+        color += mix(uColor1, uColor2, vUv.x) * density;
+        color += mix(uColor2, uColor3, vUv.y) * density;
+      }
+    }
+  }
+
+  // Apply post-processing
+  color *= 1.0 - pow(abs(vUv.x - 0.5), 10.0);
+  color *= 1.0 - pow(abs(vUv.y - 0.5), 10.0);
+  color *= smoothstep(0.0, 0.1, length(vUv - vec2(0.5)));
+
+  // Set fragment color
+  gl_FragColor = vec4(color, 1.0);
+}
+`;
+
+const fluidMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    uTime: { value: 0 },
+    uResolution: { value: new THREE.Vector2() },
+    uMouse: { value: new THREE.Vector2(10000, 10000) },
+    uDensity: { value: 0.4 },
+    uWeight: { value: 0.02 },
+    uRadius: { value: 0.1 },
+    uColor1: { value: new THREE.Color('#ff9966') },
+    uColor2: { value: new THREE.Color('#f44242') },
+    uColor3: { value: new THREE.Color('#4227f4') },
+  },
+  vertexShader,
+  fragmentShader,
+});
+
 const FluidSimulation = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    // Setup
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  const onWindowResize = () => {
     const container = containerRef.current;
-    const renderer = new THREE.WebGLRenderer();
-    const camera = new THREE.PerspectiveCamera(
-      45,
-      container.clientWidth / container.clientHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 10;
+    if (container) {
+      const width = container.clientWidth;
+      const height = container.clientHeight;
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+    }
+  };
 
-    const scene = new THREE.Scene();
-    const controls = new OrbitControls(camera, container);
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  };
 
-    // Fluid simulation parameters
-    const gridSize = 128;
-    const viscosity = 0.1;
-    const diffusion = 0.1;
-    const dt = 0.1;
-    const iterations = 16;
+  const init = () => {
+    const container = containerRef.current;
+    if (container) {
+      const width = container.clientWidth;
+      const height = container.clientHeight;
 
-    // Create the fluid simulation geometry
-    const fluidGeometry = new THREE.PlaneBufferGeometry(10, 10, gridSize, gridSize);
+      camera.position.z = 5;
 
-    // Create the velocity field
-    const velocityField = new THREE.DataTexture(
-      new Float32Array(gridSize * gridSize * 4),
-      gridSize,
-      gridSize,
-      THREE.RGBAFormat,
-      THREE.FloatType
-    );
-    velocityField.minFilter = THREE.NearestFilter;
-    velocityField.magFilter = THREE.NearestFilter;
-    velocityField.generateMipmaps = false;
+      renderer.setSize(width, height);
+      container.appendChild(renderer.domElement);
 
-    // Create the pressure field
-    const pressureField = new THREE.DataTexture(
-      new Float32Array(gridSize * gridSize),
-      gridSize,
-      gridSize,
-      THREE.LuminanceFormat,
-      THREE.FloatType
-    );
-    pressureField.minFilter = THREE.NearestFilter;
-    pressureField.magFilter = THREE.NearestFilter;
-    pressureField.generateMipmaps = false;
+      const geometry = new THREE.PlaneGeometry(2, 2);
+      const fluidMesh = new THREE.Mesh(geometry, fluidMaterial); // Utilisation du material
 
-    // Create the material for the fluid simulation
-    const fluidMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-        velocityField: { value: velocityField },
-        pressureField: { value: pressureField },
-        viscosity: { value: viscosity },
-        diffusion: { value: diffusion },
-        dt: { value: dt },
-        gridSize: { value: gridSize },
-      },
-      vertexShader: `
-        varying vec2 vUv;
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-      `,
-      fragmentShader: `
-        varying vec2 vUv;
-        uniform sampler2D velocityField;
-        uniform sampler2D pressureField;
-        uniform float viscosity;
-        uniform float diffusion;
-        uniform float dt;
-        uniform int gridSize;
+      scene.add(fluidMesh);
 
-        float getVelocity(int x, int y, int z) {
-          vec4 velocity = texture2D(velocityField, vec2(float(x) / float(gridSize), float(y) / float(gridSize)));
-          if (z == 0) {
-            return velocity.x;
-          } else if (z == 1) {
-            return velocity.y;
-          } else {
-            return velocity.z;
-          }
-        }
+      window.addEventListener('resize', onWindowResize, false);
 
-        void main() {
-          float dx = 1.0 / float(gridSize);
-          float dtvisc = viscosity * dt / (dx * dx);
-          float dtdiff = diffusion * dt / (dx * dx);
+      animate();
+    }
+  };
 
-          // Compute the Laplacian of
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  return <div ref={containerRef} style={{ width: '100%', height: '100%' }} />;
+};
+
+export default FluidSimulation;
